be500c03b4e78590f0a15797e23163d56337f018
Rodion Kovalenko <rodion.kovalenko@npo-applications.de>
2019-11-10 14:41:12 +0100

    sw

--- service-worker.js
+++ service-worker.js
@@ -1798,7 +1798,7 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
      */
     var __awaiter$5 = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
         return new (P || (P = Promise))(function (resolve, reject) {
-            function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+            function fulfilled(value) { try { if (value) { step(generator.next(value)); } } catch (e) { reject(e); } }
             function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
             function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
             step((generator = generator.apply(thisArg, _arguments || [])).next());
