90244bb65134aad18f212c3342df179ef535ec91
Rodion Kovalenko <rodion.kovalenko@npo-applications.de>
2019-11-10 14:19:31 +0100

    sw

--- service-worker.js
+++ service-worker.js
@@ -2266,7 +2266,8 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
                 this.latestHash = latest.latest;
                 // Finally, assert that the latest version is in fact loaded.
                 if (!this.versions.has(latest.latest)) {
-                    throw new Error(`Invariant violated (initialize): latest hash ${latest.latest} has no known manifest`);
+                    //throw new Error(`Invariant violated (initialize): latest hash ${latest.latest} has no known manifest`);
+					break; 
                 }
                 // Finally, wait for the scheduling of initialization of all versions in the
                 // manifest. Ordinarily this just schedules the initializations to happen during
